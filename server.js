5dd6959fed899c9142d6be8e5d51109ea70b4795
Jonas-Petrikas <jpetrik15@students.lcc.lt>
2025-01-15 16:45:58 +0200

    Create, Read - done

--- server.js
+++ server.js
@@ -26,92 +26,50 @@ const makeHTML = (data, pageName) => {
 }
 
 
-// Serve static files from the 'public' directory
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-
-    const data = {
-        pageTitle: 'Pirmasis puslapis',
-        URL
-    };
-
-    const html = makeHTML(data, 'landing');
-
-    res.send(html);
-});
-
-
-
-app.get('/read/:number', (req, res) => {
-
-
-
-    const invoice = invoices.items.find(invoice => invoice.number === req.params.number);
-
-    let subTotal = 0;
-
-    invoice.items.map(item => {
-
-        let nuolaidaProc = '';
-        let nuolaidaEur = '';
-
+function newInv(newData) {
+    newData.subTotal = 0;
+    newData.vat = 0;
+    newData.grandTotal = 0;
+    newData.totalDiscounts = 0;
+    newData.items.map(item => {
 
         if (item.discount.type === 'fixed') {
-            nuolaidaEur = item.discount.value;
-            nuolaidaProc = item.discount.value * 100 / (item.price * item.quantity);
-            item.discount.nuolaidaEur = nuolaidaEur.toFixed(2) + ' Eur';
-            item.discount.nuolaidaProc = `[${nuolaidaProc.toFixed(2)}%]`;
+            item.discount.Eur = item.discount.value;
+            item.discount.P = item.discount.value * 100 / (item.price * item.quantity);
 
         } else if (item.discount.type === 'percentage') {
-            nuolaidaProc = item.discount.value;
-            nuolaidaEur = (item.price * item.quantity - (item.price * item.quantity * (1 - nuolaidaProc / 100)));
-            item.discount.nuolaidaEur = nuolaidaEur.toFixed(2) + ' Eur';
-            item.discount.nuolaidaProc = `[${nuolaidaProc.toFixed(2)}%]`;
+            item.discount.P = item.discount.value;
+            item.discount.Eur = (item.price * item.quantity - (item.price * item.quantity * (1 - item.discount.P / 100)));
         } else {
-            nuolaidaEur = '';
-            nuolaidaProc = '';
+            item.discount.Eur = 0;
+            item.discount.P = 0;
         }
 
-        let total = (item.price * item.quantity - nuolaidaEur);
-        item.total = total.toFixed(2);
-
-        subTotal += total;
-    });
-
-    invoice.subTotal = subTotal.toFixed(2);
-    invoice.vat = ((subTotal + invoice.shippingPrice) * 0.21).toFixed(2);
-    invoice.grandTotal = ((subTotal + invoice.shippingPrice) * 1.21).toFixed(2);
 
 
+        item.itemTotal = item.quantity * item.price
+        item.itemDiscountedTotal = (item.quantity * item.price - item.discount.Eur).toFixed(2);
+        newData.subTotal += item.itemTotal;
+        newData.totalDiscounts += item.discount.Eur;
 
-    const data = {
-        pageTitle: 'Sąskaitos peržiūra',
-        invoice,
-        URL,
-    };
-
-    const html = makeHTML(data, 'read');
-
-    res.send(html);
-});
-
-app.get('/list', (req, res) => {
+        item.discount.Eur = `${item.discount.Eur.toFixed(2)}`;
+        item.discount.P = `[-${item.discount.P.toFixed(2)}%]`
 
 
-    const data = {
-        pageTitle: 'Sąskaitų sąrašas',
-        invoices,
-        URL,
-    };
+    })
+    newData.vat = ((newData.subTotal + newData.shippingPrice) * 0.21);
+    newData.grandTotal = newData.subTotal - newData.totalDiscounts + newData.shippingPrice + newData.vat;
 
-    const html = makeHTML(data, 'list');
-
-    res.send(html);
-});
-
+    newData.subTotal = newData.subTotal.toFixed(2);
+    newData.grandTotal = newData.grandTotal.toFixed(2)
+    newData.totalDiscounts = newData.totalDiscounts.toFixed(2)
+    newData.vat = newData.vat.toFixed(2)
+    newData.shippingPrice = newData.shippingPrice.toFixed(2)
 
 
+    fs.writeFileSync('./data/temp.json', JSON.stringify(newData), 'utf8');
+
+}
 
 // Function to add a new item
 function addItem(newData) {
@@ -133,12 +91,111 @@ function addItem(newData) {
     console.log(fileContent)
 }
 
-// fetch(apiUrl)
-//     .then(res => res.json())
-//     .then(data => {
-//         addItem(data);
 
-//     });
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+
+    const data = {
+        pageTitle: 'Pirmasis puslapis',
+        URL
+    };
+
+    const html = makeHTML(data, 'landing');
+
+    res.send(html);
+});
+
+
+
+app.get('/read/:number', (req, res) => {
+    let invoices = fs.readFileSync('./data/invoices.json', 'utf8');
+    invoices = JSON.parse(invoices);
+
+    const invoice = invoices.items.find(invoice => invoice.number === req.params.number);
+
+
+
+    const data = {
+        pageTitle: `Sąskaitos ${{}} peržiūra'`,
+        invoice,
+        URL,
+    };
+
+    const html = makeHTML(data, 'read');
+
+    res.send(html);
+});
+
+app.get('/list', (req, res) => {
+    let invoices = fs.readFileSync('./data/invoices.json', 'utf8');
+    invoices = JSON.parse(invoices);
+
+    const data = {
+        pageTitle: 'Sąskaitų sąrašas',
+        invoices,
+        URL,
+    };
+
+    const html = makeHTML(data, 'list');
+
+    res.send(html);
+});
+app.get('/new', (req, res) => {
+    // fetch(apiUrl)
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         newInv(data);
+    //         res.redirect(`${URL}create`);
+    //     });
+
+
+    async function getData() {
+        try {
+            const response = await fetch(apiUrl);
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+
+            const json = await response.json();
+            newInv(json);
+            console.log(json);
+            res.redirect(`${URL}create`);
+
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    getData();
+});
+
+app.get('/create', (req, res) => {
+    let invoice = fs.readFileSync('./data/temp.json', 'utf8');
+    invoice = JSON.parse(invoice);
+
+    const data = {
+        pageTitle: 'Pridėti naują',
+        invoice,
+        URL,
+    };
+
+    const html = makeHTML(data, 'create');
+
+    res.send(html);
+});
+
+app.get('/save', (req, res) => {
+    let invoice = fs.readFileSync('./data/temp.json', 'utf8');
+    invoice = JSON.parse(invoice);
+
+    addItem(invoice);
+
+    res.redirect(`${URL}list`);
+
+
+});
 
 // Start server
 
